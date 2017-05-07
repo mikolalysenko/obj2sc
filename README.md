3bba488e270ebcdb5c886ab18c87c3dc7b4c8d2f
mikolalysenko <mikolalysenko@gmail.com>
2017-05-06 19:09:09 -0700

    .

---
+++ README.md
@@ -0,0 +1,17 @@
+# obj2sc
+Command line tool that converts OBJ files to a JSON formatted simplicial complex
+
+# Install
+
+```
+npm i -g obj2sc
+```
+
+# Usage
+
+sh```
+obj2sc < mymesh.obj > mymesh.json
+```
+
+# Credits
+(c) 2017 Mikola Lysenko. MIT License
---
+++ obj2sc.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var str = []
+process.stdin
+  .setEncoding('utf-8')
+  .on('readable', function () {
+    var chunk = process.stdin.read()
+    if (chunk) {
+      str.push(chunk)
+    }
+  })
+  .on('end', function () {
+    var verts = []
+    var faces = []
+
+    str.join('').split('\n').forEach(function (line) {
+      var toks = line.split(/\s+/)
+      if (toks[0] === 'v') {
+        verts.push(toks.slice(1).map((x) => +x))
+      } else if (toks[0] === 'f') {
+        faces.push(toks.slice(1).map((x) => (x - 1) | 0))
+      }
+    })
+
+    console.log(JSON.stringify({
+      positions: verts,
+      cells: faces
+    }))
+  })
